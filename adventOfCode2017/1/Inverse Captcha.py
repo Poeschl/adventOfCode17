3e63169554e9293407eaac98ea16dd136acc6697
Markus Pöschl <markus.poeschl@pixel.de>
2017-12-08 17:10:26 +0100

    Refactored and prepared for 1-part2

--- adventOfCode2017/1/Inverse Captcha.py
+++ adventOfCode2017/1/Inverse Captcha.py
@@ -2,15 +2,14 @@ from adventOfCode2017.common.Utils import readInput
 
 inputString = readInput("input.txt")
 
-lastChar = ''
+lastChar = inputString[len(inputString) - 1]
 result = 0
 
 for char in inputString:
-    if char == lastChar and lastChar != '':
+    if char == lastChar:
         result = result + int(char)
     lastChar = char
 
-if inputString[0] == inputString[len(inputString)-1]:
-    result = result + int(inputString[len(inputString)-1])
+print("Part One: " + str(result))
 
-print(result)
+print("Part Two: " + str(result))
