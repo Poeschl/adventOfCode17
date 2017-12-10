03945611ee8922ab87c3e876a8a3ae81e272ea13
Markus Pöschl <poeschl_markus@web.de>
2017-12-10 13:26:07 +0100

    First day

--- adventOfCode2017/1/Inverse Captcha.py
+++ adventOfCode2017/1/Inverse Captcha.py
@@ -3,11 +3,17 @@ from adventOfCode2017.common.Utils import readInput, printOutput
 inputString = readInput("input.txt")
 
 lastChar = inputString[len(inputString) - 1]
-result = 0
+matches = 0
+extendedMatches = 0
+halfway = int(len(inputString) / 2)
 
-for char in inputString:
+for index, char in enumerate(inputString):
     if char == lastChar:
-        result = result + int(char)
+        matches = matches + int(char)
+
+    if char == inputString[(index + halfway) % len(inputString)]:
+        extendedMatches = extendedMatches + int(char)
+
     lastChar = char
 
-printOutput(result, result)
+printOutput(matches, extendedMatches)
