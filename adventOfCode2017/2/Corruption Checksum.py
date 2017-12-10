4e4e0eaa31d790a85d69b839bdb8475c5d58e24e
Markus Pöschl <poeschl_markus@web.de>
2017-12-10 15:57:42 +0100

    Day 2 - 2

--- adventOfCode2017/2/Corruption Checksum.py
+++ adventOfCode2017/2/Corruption Checksum.py
@@ -19,10 +19,23 @@ def getDiffOfMinAndMax(line: str):
     return max_number - min_number
 
 
+def getEvenNumbersDiff(line: str):
+    numbers = line.split(" ")
+
+    for index1, first_num in enumerate(numbers):
+        first_num = int(first_num)
+        for index2, second_num in enumerate(numbers):
+            second_num = int(second_num)
+            if first_num % second_num == 0 and first_num > second_num and index1 != index2:
+                return int(first_num / second_num)
+
+
 puzzleInput = readInputLines("input.txt")
 checksum = 0
+even_checksum = 0
 
 for line in puzzleInput:
     checksum = checksum + getDiffOfMinAndMax(line)
+    even_checksum = even_checksum + getEvenNumbersDiff(line)
 
-printOutput(checksum, 0)
+printOutput(checksum, even_checksum)
